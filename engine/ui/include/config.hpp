b004498232aff3b9cd93c621b0bc625b8de817d8
Patrick Roche <rochep63@gmail.com>
2018-06-01 00:10:13 -0500

    latest changes

--- engine/ui/include/config.hpp
+++ engine/ui/include/config.hpp
@@ -1,17 +1,19 @@
 #pragma once
 
 #include "ui_api.hpp"
-
+#include "color.hpp"
 #include <engine.hpp>
 
 namespace ui {
 
     class Config {
 	public:
-		UI_API static engine::ImageFont& getFont();
+		UI_API static engine::ImageFont* getFont();
 
 	private:
-		static engine::ImageFont font;
+		UI_API static engine::ImageFont* font;
+		UI_API static Color activeColor;
+		UI_API static Color defaultColor;
     };
 
 }
--- engine/ui/src/config.cpp
+++ engine/ui/src/config.cpp
@@ -1,8 +1,11 @@
 #include "config.hpp"
 
 namespace ui {
+	engine::ImageFont* Config::font = nullptr;
+	Color Config::activeColor = Color();
+	Color Config::defaultColor = Color();
     
-	engine::ImageFont& Config::getFont() {
+	engine::ImageFont* Config::getFont() {
 		return font;
 	}
 

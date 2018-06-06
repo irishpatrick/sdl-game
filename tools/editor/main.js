0f17e9bf0daa10edd4443adb8559215a0638fe85
Patrick Roche <rochep63@gmail.com>
2018-06-05 19:59:55 -0500

    working on new editor

---
+++ tools/editor/main.js
@@ -0,0 +1,25 @@
+const {app, BrowserWindow} = require('electron')
+let win
+
+function createWindow() {
+    win = new BrowserWindow({width: 1024, height: 768})
+    win.loadFile('app/index.html')
+    win.on('closed', () => {
+        win = null;
+    })
+    win.openDevTools();
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})

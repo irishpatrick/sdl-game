231185ecfe4506c34c36d64d951100d4309f2e2f
Patrick Roche <rochep63@gmail.com>
2018-06-18 22:02:30 -0500

    latest changes

--- engine/core/include/util.hpp
+++ engine/core/include/util.hpp
@@ -26,7 +26,6 @@ class Util
 {
 public:
     //CORE_API static uint32_t getNow();
-	CORE_API static SDL_Texture* loadTexture(const std::string&, SDL_Renderer*);
 	CORE_API static std::string checkCollision(Sprite*, Sprite*);
 	CORE_API static std::string checkVelocityCollision(Sprite*, Sprite*, float);
 	CORE_API static void contain(Sprite*, Sprite*);
@@ -36,7 +35,6 @@ public:
 	CORE_API static std::vector<Sprite*> GetCollisions(Sprite*, Group*);
 	CORE_API static std::vector<Sprite*> getVelocityCollisions(Sprite*, Group*, float);
 	CORE_API static std::vector<Sprite*> getVelocityCollisions(Sprite*, std::vector<Sprite*>&, float);
-	CORE_API static int initSDL(SDL_Window**, SDL_Renderer**, const std::string&, int, int, bool);
 	CORE_API static float lerp(float, float, float);
 };
 

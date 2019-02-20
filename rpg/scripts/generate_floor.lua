0728781a9b8dae092bdc208284f59659fa67aba1
Patrick Roche <rochep63@gmail.com>
2019-02-19 21:29:42 -0500

    working on backtracing

--- rpg/scripts/generate_floor.lua
+++ rpg/scripts/generate_floor.lua
@@ -2,64 +2,28 @@ local tools = require("tools")
 
 floor = {}
 
--- root of the backtracing recursion
-function generate_floor()
-    math.randomseed(os.time())
-    floor = tools.generate_matrix(100, 100)
-
-    backtrace(floor)
+function backtrace(mat)
+    if reject(mat) then return end
+    if accept(mat) then output(mat) end
+    
 end
 
-function backtrace(current)
-    if reject() then return end
-    if accept() then output()
-    s = first()
-    while not s == nil do
-        bt(s)
-        s = extend()
-end
-
-function reject()
+function reject(mat)
 
 end
 
-function accept()
+function accept(mat)
 
 end
 
-
-function validate(c)
-    local N = c.getn(floor)
-    local M = c.getn(floor[0])
-
-    for i=1,N do
-        for j=1,M do
-            if c[i][j] > 1 then
-                return false
-            end
-        end
-    end
-
-    return true
-end
-
-function output()
+function output(mat)
 
 end
 
-function extend()
+function first(mat)
 
 end
 
-function first()
-
-end
-
--- extend
-function place_room(size)
-
-end
-
-function connect_rooms(size)
+function next(mat)
 
 end

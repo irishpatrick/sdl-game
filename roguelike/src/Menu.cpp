3313f2b84bb7427bd5c5972cb149990f640516bb
Patrick Roche <rochep63@gmail.com>
2019-08-29 23:54:20 -0400

    begin work on menus

---
+++ roguelike/src/Menu.cpp
@@ -0,0 +1,12 @@
+#include "Menu.hpp"
+
+Menu::Menu() :
+    Sprite()
+{
+
+}
+
+Menu::~Menu()
+{
+    
+}
---
+++ roguelike/src/Menu.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <engine.hpp>
+
+using namespace engine;
+
+class Menu : public Sprite
+{
+public:
+    Menu();
+    ~Menu();
+};

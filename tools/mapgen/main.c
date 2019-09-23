eb908e46400e264bd43175cb2fc90b82c55d7471
Patrick Roche <rochep63@gmail.com>
2019-09-23 00:35:56 -0400

    working on map generator

--- tools/mapgen/main.c
+++ tools/mapgen/main.c
@@ -4,6 +4,7 @@
 #include "grid.h"
 #include "cell.h"
 #include "cellstack.h"
+#include "point.h"
 
 int randint(int min, int max)
 {
@@ -14,34 +15,95 @@ void gen_mods(int* arr)
 {
     int r = randint(0, 3);
     int i;
+    for (int i = 0; i < 8; i += 2)
+    {
+        int dir = (r + i) % 4;
+        int* x = &arr[i];
+        int* y = &arr[i+1];
+
+        switch (dir)
+        {
+            case 0:
+                *x = 1;
+                *y = 0;
+                break;
+            case 1:
+                *x = 0;
+                *y = 1;
+                break;
+            case 2:
+                *x = -1;
+                *y = 0;
+                break;
+            case 3:
+                *x = 0;
+                *y = -1;
+                break;
+        }
+    }
 }
+
+int reject(struct Grid* grid)
+{
+
+}
+
+int accept(struct Grid* grid)
+{
+
+}
+
+void next(struct Grid* grid, Point point)
+{
+
+}
+
+Point first(struct Grid* grid, Point point)
+{
+    Point ext;
+
+    return ext;
+}
+
+void backtrace(struct Grid* grid, Point pt)
+{
+    if (reject(grid))
+    {
+        return;
+    }
+
+    if (accept(grid))
+    {
+        // done
+        return;
+    }
+
+    Point point = first(grid);
+    while (point.x >= 0 && point.y >= 0)
+    {
+        backtrace(grid, point);
+        point = next(grid, point);
+    }    
+}
+
+/**
+ * plan
+ * pick start cell
+ * pick next
+ * validate
+ * next
+ * validate
+ * next validate
+ * 
+ * if validate fails
+ * return
+ */
 
 int main(int argc, char** argv)
 {
     srand(time(NULL));
 
-    int length = 32;
-
-    struct Grid grid;
-    grid_new(&grid, length);
-
-    int x = 10;
-    int y = 0;
-
-    struct CellStack cs;
-    cs_init(&cs);
-
-    cs_push(&cs, grid_get(&grid, x, y));
-
-    //cs_print(&cs);
-
-    int mods[8];
-
-    while (cs_size(&cs) > 0)
-    {
-        int nx = x;
-        int ny = y;
-    }
+    
 
     return 0;
 }
